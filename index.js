50007d0a751b0bec0cbc0884311cc87c3094aee1
VishK98 <visheshkumar41@gmail.com>
2024-09-22 13:07:06 +0530

    cors fixed

--- index.js
+++ index.js
@@ -7,17 +7,16 @@ const errorHandler = require("./middleware/errorsMiddleware");
 const cookieParser = require("cookie-parser");
 const app = express();
 
-app.use(morgan("tiny"));
-app.use(cors());
-app.use(cookieParser());
+dotenv.config({ path: ".env" });
 
+app.use(morgan("tiny"));
+app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-dotenv.config({ path: ".env" });
-
+// CORS Middleware
 app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:4000");
+  res.setHeader("Access-Control-Allow-Origin", "*"); // Allow all origins for testing
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Credentials", "true");
@@ -25,10 +24,10 @@ app.use((req, res, next) => {
   next();
 });
 
+// Routes
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/category", require("./routes/categoryRoutes"));
 app.use("/api/product", require("./routes/productRoutes"));
-// app.use("/api/upload",require("./utils/uploadImage"))
 
 const PORT = process.env.PORT || 4000;
 
@@ -36,6 +35,5 @@ app.use(errorHandler);
 connectDB();
 
 app.listen(PORT, () => {
-  7;
-  console.log(`server started on http://localhost:${PORT}`);
+  console.log(`Server started on http://localhost:${PORT}`);
 });
